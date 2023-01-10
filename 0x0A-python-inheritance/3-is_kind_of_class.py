f9206d53bda54b44c9827e2553975a5e8fa54cf7
Dyuuz <lekanoyesunle@gmail.com>
2023-01-10 05:56:22 -0800

    Write a class MyList that inherits from list:

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-"""
-Multiple inheritance type
-"""
-
-
 def is_kind_of_class(obj, a_class):
     """
-    a function that returns True if the object
-    is an instance of, or if the object is an instance
-    of a class that inherited from, the specified class;
-    otherwise False
+    Checks if `obj` is the same class or inherit from `a_class`
+    Args:
+        obj (any): The object to compare
+        a_class (any): The class to compare with the object
+    Returns:
+        `True` if the object is an instance or inherit from the
+        specified class; otherwise `False`
     """
-    if isinstance(obj, a_class):
-        return True
-    return False
+
+    return isinstance(obj, a_class)
